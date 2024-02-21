8c6868e2f649a1f3b15ff03e650473dc11d2e96b
kozka.c <corentin.kozk@gmail.com>
2024-02-21 10:23:03 +0100

    feat(etl): input keyword

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -1,8 +1,9 @@
 import argparse
 def main():
     parser = argparse.ArgumentParser(description="ETL en ligne de commande en Python")
-    parser.add_argument('--input_file',help='Chemin du fichier d\'entrée', required=False)
-    parser.add_argument('--output_file', help='Chemin du fichier de sortie', required=True)
+    parser.add_argument('--input_file',help='Chemin du fichier d\'entrée', required=True)
+    parser.add_argument('--output_file', help='Chemin du fichier de sortie', required=False, default='json')
+    parser.add_argument('--keyword_file', help='Liste des Key à link=', nargs='+')
 
     args = parser.parse_args()
     print(args)
