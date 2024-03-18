8c46e2f61a08abc3ea7b6bd8bdeaf1df646998aa
kozka.c <corentin.kozk@gmail.com>
2024-03-18 17:46:09 +0100

    feat : add multi input
    Signed-off-by: kozka.c <corentin.kozk@gmail.com>

--- src/services/extract/extractor_handler.py
+++ src/services/extract/extractor_handler.py
@@ -1,5 +1,6 @@
 from .getApi import getApi
 from ..core.data_frame import DataFrame
+import pandas as pd
 
 
 def order_handler(order_name, directive):
@@ -12,7 +13,10 @@ def extract_handler(orders):
     df = None
     for order_id, order_data in orders.items():
         result = order_handler(order_id, order_data)
-        df = DataFrame(result)
+        if df is not None:
+            df = pd.concat(df, DataFrame(result))
+        else:
+            df = DataFrame(result)
     if df is None:
         raise Exception('No data found')
     return df
