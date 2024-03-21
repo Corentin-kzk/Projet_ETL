697334f3e600315dd109550db23ada365c796d88
kozka.c <corentin.kozk@gmail.com>
2024-03-21 14:19:32 +0100

    feat: add csv + json connector
    Signed-off-by: kozka.c <corentin.kozk@gmail.com>

--- src/services/extract/getApi.py
+++ src/services/extract/getApi.py
@@ -1,14 +1,14 @@
 import requests
-import json
-
-url_dnd5eapi = "https://www.dnd5eapi.co/api/monsters"
 
 headers = {
     'Accept': 'application/json'
 }
 
 
-def get_api(url: str):
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    return data
+def get_api(path: str):
+    try:
+        response = requests.get(path, headers)
+        data = response.json()
+        return data
+    except Exception:
+        raise Exception
